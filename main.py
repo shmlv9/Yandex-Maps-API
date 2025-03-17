1e2d9ac3e7ec6c352401c2c12c65b3e155766792
shmlv9 <denis2009shumilov@gmail.com>
2025-03-17 11:59:02 +0300

    Task 2

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 import requests
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget
 from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import Qt
 from apikey import API_KEY_STATIC
 
 
@@ -10,21 +11,31 @@ class MapApp(QWidget):
     def __init__(self):
         super().__init__()
         self.init_ui()
+        self.spn = [5, 5]
+        self.lonlat = [30, 60]
+        self.min_spn = 0.001
+        self.max_spn = 90
 
     def init_ui(self):
         self.setWindowTitle('Yandex Maps')
         self.setGeometry(100, 100, 600, 450)
+        self.label = QLabel(self)
+        self.label.resize(600, 450)
 
     def show_map(self, map_file):
-        self.label = QLabel(self)
         self.pixmap = QPixmap(map_file)
         self.label.setPixmap(self.pixmap)
         os.remove(map_file)
 
-    def get_map(self, lonlat, spn):
+    def get_map(self, lonlat=None, spn=None):
+        if lonlat is None:
+            lonlat = self.lonlat
+        if spn is None:
+            spn = self.spn
+
         map_params = {
-            'll': lonlat,
-            'spn': spn,
+            'll': ','.join(map(str, lonlat)),
+            'spn': ','.join(map(str, spn)),
             'apikey': API_KEY_STATIC
         }
         map_api_server = "https://static-maps.yandex.ru/v1"
@@ -41,10 +52,20 @@ class MapApp(QWidget):
             file.write(response.content)
         self.show_map(map_file)
 
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_Plus:
+            self.spn = [max(self.min_spn, self.spn[0] * 0.5), max(self.min_spn, self.spn[1] * 0.5)]
+        elif event.key() == Qt.Key.Key_Minus:
+            self.spn = [min(self.max_spn, self.spn[0] * 2), min(self.max_spn, self.spn[1] * 2)]
+        self.get_map()
+
+    def resizeEvent(self, event):
+        self.label.resize(self.width(), self.height())
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MapApp()
-    ex.get_map('30,60', '5,5')
+    ex.get_map()
     ex.show()
     sys.exit(app.exec())
